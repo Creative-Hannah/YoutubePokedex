6065b9efd2bfea19d215a93e5d37243b54a53d67
Creative-Hannah <81365391+Creative-Hannah@users.noreply.github.com>
2021-06-19 11:13:45 +0530

    First Commit Removing Existing Code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React from "react";
-import Pokedex from "./Pokedex";
-import Pokemon from "./Pokemon";
 import { Route, Switch } from "react-router-dom";
-
 const App = () => (
   <Switch>
-    <Route exact path="/" render={(props) => <Pokedex {...props} />} />
-    <Route
-      exact
-      path="/:pokemonId"
-      render={(props) => <Pokemon {...props} />}
-    />
+    <div />
   </Switch>
 );
 
